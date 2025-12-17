79b82233a4542cb1f5a2b946b712f724dde01a84
Tanishq Gupta <guptatanishq131@gmail.com>
2025-12-17 15:04:30 +0530

    Program to check number is positive,negative,zero

---
+++ leet-code-codebase/checknumber.java
@@ -0,0 +1,14 @@
+class Checknumber
+    public static void main(String[] args) {
+        int num = -5;
+
+        // Checking number type
+        if (num > 0) {
+            System.out.println("Positive number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
