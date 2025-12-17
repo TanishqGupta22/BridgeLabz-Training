78e229698d41e345854f39a07f34ee653228625f
Tanishq Gupta <guptatanishq131@gmail.com>
2025-12-17 16:28:18 +0530

    program to find average of three numbers

---
+++ gcr-codebase/programming-elements/averageofthree.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class AverageOfThree {
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter first number
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        // Ask user to enter second number
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Ask user to enter third number
+        System.out.print("Enter third number: ");
+        double num3 = sc.nextDouble();
+
+        // Calculate average
+        double average = (num1 + num2 + num3) / 3;
+
+        // Display the result
+        System.out.println("Average of three numbers = " + average);
+    }
+}
