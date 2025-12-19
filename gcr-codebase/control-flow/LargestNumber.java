aea6b25c8a409026b78636e2d37a54b3dfde74ae
Tanishq Gupta <guptatanishq131@gmail.com>
2025-12-19 15:29:37 +0530

    Program to find largest of three number added

--- gcr-codebase/control-flow/LargestNumber.java
+++ gcr-codebase/control-flow/LargestNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FirstSmallest {
+public class LargestNumber{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,8 +8,12 @@ public class FirstSmallest {
         int number2 = sc.nextInt();
         int number3 = sc.nextInt();
 
-        boolean result = (number1 < number2 && number1 < number3);
+        boolean firstLargest = (number1 > number2 && number1 > number3);
+        boolean secondLargest = (number2 > number1 && number2 > number3);
+        boolean thirdLargest = (number3 > number1 && number3 > number2);
 
-        System.out.println("Is the first number the smallest? " + result);
+        System.out.println("Is the first number the largest? " + firstLargest);
+        System.out.println("Is the second number the largest? " + secondLargest);
+        System.out.println("Is the third number the largest? " + thirdLargest);
     }
 }
