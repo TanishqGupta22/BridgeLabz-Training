024718e7c7551b12583422245be7a0a2f306d5b4
Tanishq Gupta <131939678+TanishqGupta22@users.noreply.github.com>
2026-01-30 11:00:34 +0530

    [Tanishq Gupta] add.Refactor GoodsTransport class to be abstract

--- oops-practice/scenerio-based/FutureLogistics/src/com/futurelogistics/GoodsTransport.java
+++ oops-practice/scenerio-based/FutureLogistics/src/com/futurelogistics/GoodsTransport.java
@@ -1,5 +1,43 @@
-package com.futurelogistics;
+public abstract class GoodsTransport {
 
-public class GoodsTransport {
+    // Protected attributes
+    protected String transportId;
+    protected String transportDate;
+    protected int transportRating;
 
+    // 3-argument constructor
+    public GoodsTransport(String transportId, String transportDate, int transportRating) {
+        this.transportId = transportId;
+        this.transportDate = transportDate;
+        this.transportRating = transportRating;
+    }
+
+    // Getters and Setters
+    public String getTransportId() {
+        return transportId;
+    }
+
+    public void setTransportId(String transportId) {
+        this.transportId = transportId;
+    }
+
+    public String getTransportDate() {
+        return transportDate;
+    }
+
+    public void setTransportDate(String transportDate) {
+        this.transportDate = transportDate;
+    }
+
+    public int getTransportRating() {
+        return transportRating;
+    }
+
+    public void setTransportRating(int transportRating) {
+        this.transportRating = transportRating;
+    }
+
+    // Abstract methods
+    public abstract String vehicleSelection();
+    public abstract float calculateTotalCharge();
 }
