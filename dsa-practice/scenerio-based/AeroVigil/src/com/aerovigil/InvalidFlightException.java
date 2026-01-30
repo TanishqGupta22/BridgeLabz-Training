41ed79646d46177ac704066a2a1f38a1bc6fc415
Tanishq Gupta <131939678+TanishqGupta22@users.noreply.github.com>
2026-01-30 10:55:11 +0530

    [Tanishq Gupta] add .Extend InvalidFlightException from Exception class

--- dsa-practice/scenerio-based/AeroVigil/src/com/aerovigil/InvalidFlightException.java
+++ dsa-practice/scenerio-based/AeroVigil/src/com/aerovigil/InvalidFlightException.java
@@ -1,5 +1,7 @@
 package com.aerovigil;
 
-public class InvalidFlightException {
-
+public class InvalidFlightException extends Exception {
+    public InvalidFlightException(String message) {
+        super(message);
+    }
 }
